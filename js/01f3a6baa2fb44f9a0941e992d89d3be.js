41687c52f99f5fc760743755a64dc0f16ad84023
huang379428449@126.com <huang379428449@126.com>
2017-10-01 23:29:27 +0800

    本地实现复刻

---
+++ js/01f3a6baa2fb44f9a0941e992d89d3be.js
@@ -0,0 +1,17 @@
+pageData = {
+    "share": {
+        "shareTitle": "飞越淘宝奇市",
+        "shareText": "＃淘宝造物节＃我刚刚在奇市江湖完成了一次高空飞行，电影级特效的淘宝奇市限时开启，快来和我一起体验https://huodong.m.taobao.com/act/4lxibc.html",
+        "sharePic": "//gw.alicdn.com/mt/TB1Xak0RVXXXXcBXVXXXXXXXXXX-750-2467.jpg",
+        "shareUrl": "https://g.alicdn.com/fdilab/flyover-taobao-online/index.html"
+    },
+    "saleLink": "http://huodong.m.taobao.com/act/2017taobaomaker.html?spm=a1z3i.a4.0.0.cjpoV2",
+    "downGradeLink": "https://g.alicdn.com/fdilab/app-flyover-taobao-video/0.1.3/index.html",
+    "normalVideos": {
+        "east": "./video/LB11EkORVXXXXagXXXXXXXXXXXX.mp4",
+        "west": "./video/LB1TxQeRVXXXXc7XVXXXXXXXXXX.mp4",
+        "south": "./video/LB11qAMRVXXXXcEXXXXXXXXXXXX.mp4",
+        "north": "./video/LB16ukNRVXXXXaKXXXXXXXXXXXX.mp4"
+    },
+    "lowLink": ""
+}
